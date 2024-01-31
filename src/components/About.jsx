cde0faef3997ad2b5a92a58e99ea839ff0275450
rohith dandi <rohith.dandi1712@gmail.com>
2024-02-01 00:47:25 +0530

    main

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,16 +5,16 @@ const About = () => {
   return (
     <div
       id="about"
-      className="flex mx-auto border-solid border-x-8 border-[#191919] text-[#f9f4f0] w-full md:w-3/4 md:bg-[#111111]"
+      className="flex mx-auto border-solid border-x-8 border-[#191919] text-[#f9f4f0] w-full md:w-3/4 sm:bg-[#111111]"
     >
-      <div className="flex flex-col w-full lg:flex-row justify-center items-center py-8 md:bg-[#111111]">
+      <div className="flex flex-col w-full lg:flex-row justify-center items-center py-8">
         <div className="flex justify-center items-center w-full font-azeret p-4 ">
-          <div className="flex-1 flex w-full max-w-[320px] lg:max-w-[500px] text-xl md:text-2xl">
+          <div className="flex-1 flex w-full max-w-[320px] lg:max-w-[500px] text-xl md:text-2xl bg-[#111111]">
             Hello, I am a Final Year Undergraduate at Mahindra University Ecole
             Centrale Pursuing Major In CSE Department.
           </div>
         </div>
-        <div className="w-1/2 lg:w-full bg-[#111111]">
+        <div className="w-1/2 lg:w-full">
           <img className="flex-1" src={dp} alt="/" />
         </div>
       </div>
