54733c513632ab86ed802a95dea732bc55246637
rohith dandi <rohith.dandi1712@gmail.com>
2024-02-26 22:15:11 +0530

    main

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/Portfolio" element={<Home />}></Route>
+          <Route path="/" element={<Home />}></Route>
         </Routes>
       </Router>
     </div>
